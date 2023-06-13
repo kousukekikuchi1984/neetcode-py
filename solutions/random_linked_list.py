7b5d8dd312b4ee31a9887154195a71651cc64d9c
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-06-14 06:25:25 +0900

    added random_linked_list

--- solutions/random_linked_list.py
+++ solutions/random_linked_list.py
@@ -7,4 +7,19 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        pass
+        if head is None:
+            return None
+        copy = {}
+        cur = head
+        while cur:
+            copy[cur] = Node(x=cur.val, next=cur.next, random=cur.random)
+            cur = cur.next
+
+        dummy = Node(x=0, next=head)
+        cur = dummy
+        while cur:
+            cur.next = copy.get(cur.next)
+            cur.random = copy.get(cur.random)
+            cur = cur.next
+
+        return copy[head]
