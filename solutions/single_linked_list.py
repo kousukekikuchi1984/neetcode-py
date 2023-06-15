1f92abb283f46bbe2cb4fbd4d1538201db2fcc25
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-06-16 05:50:26 +0900

    added reverseKGroup but failed at leetcode

--- solutions/single_linked_list.py
+++ solutions/single_linked_list.py
@@ -1,5 +1,5 @@
 import heapq
-from typing import Optional
+from typing import Optional, List
 
 
 class ListNode:
@@ -44,3 +44,26 @@ class Solution:
             current_node = current_node.next
             current_node.val = val
         return first_node.next
+
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if k == 1 or not head:
+            return head
+
+        stack = []
+        dummy = ListNode(next=head)
+        prev = dummy
+        cur = head
+
+        while cur:
+            for _ in range(k):
+                if not cur:
+                    break
+                stack.append(cur)
+                cur = cur.next
+
+            if len(stack) == k:
+                while stack:
+                    prev.next = stack.pop()
+                    prev = prev.next
+                prev.next = cur
+        return dummy.next
--- tests/test_single_linked_list.py
+++ tests/test_single_linked_list.py
@@ -18,3 +18,21 @@ class TestSolution:
             a = a.next
         expected_lis = [1, 2, 3, 5]
         assert actual_lis == expected_lis
+
+    def test_reverseKGroup(self):
+        head = ListNode(
+            val=1,
+            next=ListNode(
+                val=2, next=ListNode(val=3, next=ListNode(val=4, next=ListNode(val=5)))
+            ),
+        )
+        solution = Solution()
+        actual = solution.reverseKGroup(head, 2)
+        actual_lis = []
+        a = actual
+        while a:
+            actual_lis.append(a.val)
+            a = a.next
+        expected_lis = [2, 1, 4, 3, 5]
+        assert actual_lis == expected_lis
+
