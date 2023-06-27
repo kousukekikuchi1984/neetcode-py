f3113bb430d1a7fdee2171c3f7c171cee8860743
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-06-28 06:08:45 +0900

    added partition

--- solutions/backtrack.py
+++ solutions/backtrack.py
@@ -24,3 +24,33 @@ class Solution:
         candidates.sort()
         dfs([], 0, 0)
         return results
+
+    def partition(self, s: str) -> List[List[str]]:
+        def is_palindrome(s: str):
+            left = 0
+            right = len(s) - 1
+            while left < right:
+                leftc = s[left]
+                rightc = s[right]
+                if leftc != rightc:
+                    return False
+                left += 1
+                right -= 1
+            return True
+
+        resp = []
+        partition = []
+
+        def dfs(idx: int):
+            if idx >= len(s):
+                resp.append(partition.copy())
+                return
+            for j in range(idx, len(s)):
+                newstr = s[idx : j + 1]
+                if is_palindrome(newstr):
+                    partition.append(newstr)
+                    dfs(j + 1)
+                    partition.pop()
+
+        dfs(0)
+        return resp
