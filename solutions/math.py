8febbd9e52a9432ff08eebcae09b4d9da664904b
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-07-02 12:49:32 +0900

    added isHappy

---
+++ solutions/math.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        if n == 0:
+            return False
+        already_visits = set()
+            
+        def sum_squared_digits(n: int) -> int:
+            sum = 0
+            while n:
+                remaining = n % 10
+                sum += remaining ** 2
+                n = n // 10
+            return sum
+
+        while True:
+            if n == 1:
+                return True
+            elif n in already_visits:
+                return False
+
+            already_visits.add(n)
+            n = sum_squared_digits(n)
