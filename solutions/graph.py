ee758965a19eba7546b30cdf84915abeded124ba
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-07-05 07:10:02 +0900

    added solve

--- solutions/graph.py
+++ solutions/graph.py
@@ -1,3 +1,5 @@
+from typing import Dict, Tuple
+
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
@@ -20,3 +22,29 @@ class Solution:
             return copied
 
         return dfs(node) if node else None
+
+    def solve(self, board: List[List[str]]) -> None:
+        rows, cols = len(board), len(board[0])
+
+        def capture(r, c):
+            if not 0 < r <= rows or not 0 < c <= cols or board[r][c] != "0":
+                return
+            board[r][c] = "T"
+            capture(r + 1, c)
+            capture(r - 1, c)
+            capture(r, c + 1)
+            capture(r, c - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O" and (r in [0, rows - 1] or c in [0, cols - 1]):
+                    capture(r, c)
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
+                else:
+                    continue
