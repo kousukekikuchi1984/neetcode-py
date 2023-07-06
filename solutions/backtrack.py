4fc2c5ba6c342602cb3d9f40cfa225491a05be9d
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-07-06 16:40:53 +0900

    addee solveNQueens

--- solutions/backtrack.py
+++ solutions/backtrack.py
@@ -54,3 +54,36 @@ class Solution:
 
         dfs(0)
         return resp
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["."] * n for i in range(n)]
+        res = []
+        col = set()
+        pos_diag = set()
+        neg_diag = set()
+
+        def dfs(r: int):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            #
+            for c in range(n):
+                if c in col or (r + c) in pos_diag or (r - c) in neg_diag:
+                    continue
+
+                col.add(c)
+                pos_diag.add(r + c)
+                neg_diag.add(r - c)
+                board[r][c] = "Q"
+
+                # backtrack
+                dfs(r + 1)
+
+                col.remove(c)
+                pos_diag.remove(r + c)
+                neg_diag.remove(r - c)
+                board[r][c] = "."
+
+        dfs(0)
+        return res
