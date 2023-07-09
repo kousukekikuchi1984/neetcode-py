224ebd4ebe762b5d3cb27bbc35f769afe654baae
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-07-10 06:23:21 +0900

    fixed numDecodings

--- solutions/dynamic_programming.py
+++ solutions/dynamic_programming.py
@@ -19,22 +19,18 @@ class Solution:
         return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
 
     def numDecodings(self, s: str) -> int:
-        if s[0] == "0":
-            return 0
-        max_idx = len(s) - 1
-        res = 1
-        trans_1 = set(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
-        trans_2 = set(["1", "2", "3", "4", "5", "6"])
-        for i in range(len(s)):
-            c = s[i]
-            match c:
-                case "1" | "2":
-                    # we must check next value
-                    # 1 -> 1-9 then increment res
-                    # 2 -> 1-6 then increment res
-                    next_id = i + 1
-                    if next_id <= max_idx and ((c == "1" and s[next_id] in trans_1) or (c == "2" and s[next_id] in trans_2)):
-                        res += 1
-                case _:
-                    continue
-        return res
+        dp = {len(s): 1}
+
+        def dfs(i: int) -> int:
+            if i in dp:
+                return dp[i]
+            if s[i] == "0":
+                return 0
+
+            res = dfs(i + 1)
+            if i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"):
+                res += dfs(i + 2)
+            dp[i] = res
+            return res
+
+        return dfs(0)
