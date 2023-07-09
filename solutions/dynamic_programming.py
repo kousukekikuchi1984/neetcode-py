d58ee5f1593878bc9cd05aa9dc5050342226abae
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-07-10 05:33:29 +0900

    added numDecodings but need to be refactored

--- solutions/dynamic_programming.py
+++ solutions/dynamic_programming.py
@@ -17,3 +17,24 @@ class Solution:
             return rob2
 
         return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
+
+    def numDecodings(self, s: str) -> int:
+        if s[0] == "0":
+            return 0
+        max_idx = len(s) - 1
+        res = 1
+        trans_1 = set(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
+        trans_2 = set(["1", "2", "3", "4", "5", "6"])
+        for i in range(len(s)):
+            c = s[i]
+            match c:
+                case "1" | "2":
+                    # we must check next value
+                    # 1 -> 1-9 then increment res
+                    # 2 -> 1-6 then increment res
+                    next_id = i + 1
+                    if next_id <= max_idx and ((c == "1" and s[next_id] in trans_1) or (c == "2" and s[next_id] in trans_2)):
+                        res += 1
+                case _:
+                    continue
+        return res
