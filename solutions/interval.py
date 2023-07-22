d6dc812b9a51b88d3e062181eaec79c50b0f844c
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-07-23 06:24:30 +0900

    added merge

--- solutions/interval.py
+++ solutions/interval.py
@@ -11,3 +11,17 @@ class Solution:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         merged.append(newInterval)
         return merged
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals, key=lambda x: x[0])
+        start, end = intervals[0][0], intervals[0][1]
+        merged = []
+        for i in range(1, len(intervals)):
+            if end < intervals[i][0]:
+                merged.append([start, end])
+                start, end = intervals[i][0], intervals[i][1]
+                continue
+            if end < intervals[i][1]:
+                end = intervals[i][1]
+        merged.append([start, end])
+        return merged
