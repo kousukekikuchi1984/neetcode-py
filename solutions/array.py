fe63552b4027195e1c8fabdaf2edbc06ad84a5bf
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-07-28 04:48:13 +0900

    add kthLargestNumber

--- solutions/array.py
+++ solutions/array.py
@@ -75,3 +75,7 @@ class Solution:
             emails[cur] = f"{local}@{domain}"
             cur += 1
         return set(emails)
+
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        nums = sorted(nums, key=lambda x: int(x))
+        return nums[-k]
