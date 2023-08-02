c056c25c466d24ba6755a4c5e29271452ecbf647
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-08-02 09:34:37 +0900

    add checkPossibility

--- solutions/array.py
+++ solutions/array.py
@@ -141,3 +141,20 @@ class Solution:
 
     def multiply(self, num1: str, num2: str) -> str:
         return str(int(num1) * int(num2))
+
+    def checkPossibility(self, nums: List[int]) -> bool:
+        if len(nums) == 2:
+            return True
+
+        changed = False
+        for i, num in enumerate(nums):
+            if i == len(nums) - 1 or num <= nums[i + 1]:
+                continue
+            if changed:
+                return False
+            if i == 0 or nums[i + 1] >= nums[i - 1]:
+                nums[i] = nums[i + 1]
+            else:
+                nums[i + 1] = nums[i]
+            changed = True
+        return True
