12d241d1f7402df60a4e29a57e57dd341cfcb891
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-08-17 06:30:11 +0900

    fixed minCostClimbingStairs

--- solutions/dynamic_programming.py
+++ solutions/dynamic_programming.py
@@ -3,9 +3,11 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        for i in range(len(cost) - 3, -1, -1):
-            cost[i] += min(cost[i + 1], cost[i + 2])
-        return min(cost[0], cost[1])
+        dp = [0] * (len(cost))
+        dp[0] = cost[0]; dp[1] = cost[1]
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+        return min(dp[len(cost) - 1], dp[len(cost) - 2])
 
     def rob(self, nums: List[int]) -> int:
         def helper(nums: List[int]):
