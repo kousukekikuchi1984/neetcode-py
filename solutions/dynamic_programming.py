da4ef0041e52bde95e8a5ae627628fe2b172760a
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-08-20 06:14:13 +0900

    add coinChange

--- solutions/dynamic_programming.py
+++ solutions/dynamic_programming.py
@@ -319,3 +319,14 @@ class Solution:
                 else:
                     break
         return chars
+
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                if dp[i - coin] != float("inf"):
+                    dp[i] = min(dp[i - coin] + 1, dp[i])
+        return dp[amount] if dp[amount] != float("inf") else -1
