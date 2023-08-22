9fa099e45a54f76875bb7c2677db3037a45541c6
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-08-23 06:34:56 +0900

    add mySqrt

--- solutions/math.py
+++ solutions/math.py
@@ -47,3 +47,14 @@ class Solution:
             x = int(x // 10)
 
         return True
+
+    def mySqrt(self, x: int) -> int:
+        left = 0
+        right = x
+        while right >= left:
+            mid = (left + right) // 2
+            if mid * mid <= x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
