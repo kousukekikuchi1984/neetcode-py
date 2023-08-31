1fb6b8da7e6dd584c24398a4f59a829a7fa56c3d
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-09-01 06:46:09 +0900

    add validPartition

--- solutions/array.py
+++ solutions/array.py
@@ -369,3 +369,22 @@ class Solution:
             if binary not in numset:
                 return binary
         assert False, "*** unreachable"
+
+    def validPartition(self, nums: List[int]) -> bool:
+        dp = {}
+
+        def dfs(i: int) -> bool:
+            if i == len(nums):
+                return True
+            if i in dp:
+                return dp[i]
+
+            res = False
+            if i < len(nums) - 1 and nums[i] == nums[i + 1]:
+                res = dfs(i + 2)
+            if i < len(nums) - 2:
+                if (nums[i] == nums[i + 1] == nums[i + 2] or nums[i] + 1 == nums[i + 1] == nums[i + 2] - 1):
+                    res = max(res, dfs(i + 3))
+            dp[i] = res
+            return res
+        return dfs(0)
