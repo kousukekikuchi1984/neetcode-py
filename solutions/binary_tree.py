2a663169a40b3540360ba6b90441a1d81e32253c
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2023-09-17 07:17:00 +0900

    add findPeakElement

--- solutions/binary_tree.py
+++ solutions/binary_tree.py
@@ -27,3 +27,15 @@ class Solution:
                 left = mid + 1
 
         return res
+
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if mid > 0 and nums[mid] < nums[mid - 1]:
+                right = mid - 1
+            elif mid < len(nums) - 1 and nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                return mid
